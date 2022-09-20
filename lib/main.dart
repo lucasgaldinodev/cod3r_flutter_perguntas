72d25ad1c0f0345146426c40ccc84080e9fdd063
Lucas Galdino <lucasgaldino7@gmail.com>
2022-09-20 09:15:15 -0300

    30_construindo_o_primeiro_widget_02

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const PerguntaApp());
 }
 
-class MyApp extends StatelessWidget {
-  var perguntaSelecionada = 0;
-
-  MyApp({super.key});
-
-  void responder() {
-    perguntaSelecionada++;
-    print(perguntaSelecionada);
-  }
+class PerguntaApp extends StatelessWidget {
+  const PerguntaApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final perguntas = [
-      'Qual é a sua cor favorita?',
-      'Qual é o seu animal favorito?',
-    ];
-
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Perguntas'),
-        ),
-        body: Column(
-          children: [
-            Text(perguntas[perguntaSelecionada]),
-            ElevatedButton(
-              onPressed: responder,
-              child: const Text('Resposta 1'),
-            ),
-            ElevatedButton(
-              onPressed: responder,
-              child: const Text('Resposta 2'),
-            ),
-            ElevatedButton(
-              onPressed: responder,
-              child: const Text('Resposta 3'),
-            ),
-          ],
-        ),
-      ),
+    return const MaterialApp(
+      home: Text('Olá Flutter!!!'),
     );
   }
 }
