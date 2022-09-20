c75bd8b69dd6ea1919a8cd0715c7430c6885981b
Lucas Galdino <lucasgaldino7@gmail.com>
2022-09-20 08:52:23 -0300

    32_arvore_de_widgets

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Text('Olá Flutter!!!'),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Perguntas'),
+        ),
+        body: const Text('Olá Flutter!!!'),
+      ),
     );
   }
 }
