85799c40c8ede3a5c7a7a26715be95e99f0d0666
Lucas Galdino <lucasgaldino7@gmail.com>
2022-09-21 08:50:52 -0300

    35_usando_column_para_organizar_os_widgets

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,33 @@ class PerguntaApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Perguntas'),
         ),
-        body: const Text('Olá Flutter!!!'),
+        body: Column(
+          children: [
+            Text(perguntas[0]),
+            const ElevatedButton(
+              onPressed: null,
+              child: Text('Resposta 1'),
+            ),
+            const ElevatedButton(
+              onPressed: null,
+              child: Text('Resposta 2'),
+            ),
+            const ElevatedButton(
+              onPressed: null,
+              child: Text('Resposta 3'),
+            ),
+          ],
+        ),
       ),
     );
   }
